272c058e77f23963bd7912a4b559ec21bdf053c9
Sean <seon2you@hotmail.com>
2017-07-09 22:49:14 -0700

    custom routes

--- VidlyMovie/Controllers/MoviesController.cs
+++ VidlyMovie/Controllers/MoviesController.cs
@@ -39,5 +39,10 @@ namespace VidlyMovie.Controllers
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
