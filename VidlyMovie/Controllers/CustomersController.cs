b95f395840c2740bcff190ab1d560e8f23df7960
Sean <seon2you@hotmail.com>
2017-07-11 07:47:56 -0700

    customer model binding

--- VidlyMovie/Controllers/CustomersController.cs
+++ VidlyMovie/Controllers/CustomersController.cs
@@ -61,5 +61,14 @@ namespace VidlyMovie.Controllers
 
             return View(viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
     }
 }
